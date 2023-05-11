6a9ed406e92d194e51910dd641e929a088fba49e
KimDoHwan <kdh@globalzeus.com>
2023-05-11 14:31:01 +0900

    [INIT]

---
+++ Calculator/calculator.cpp
@@ -0,0 +1,18 @@
+﻿#include "calculator.h"
+
+int calculator::plus(int a, int b)
+{
+	return 0;
+}
+int calculator::minus(int a, int b)
+{
+	return 0;
+}
+int calculator::division(int a, int b)
+{
+	return 0;
+}
+int calculator::multiply(int a, int b)
+{
+	return 0;
+}
---
+++ Calculator/calculator.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+
+class calculator
+{
+public:
+	static int plus(int a, int b);
+	static int minus(int a, int b);
+	static int division(int a, int b);
+	static int multiply(int a, int b);
+};
